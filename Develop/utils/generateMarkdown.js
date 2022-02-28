900a66feb1fdebe4f81ecd0b75df1876d808aef0
Andy MCA <AndrewMcAlpin@gmail.com>
2022-02-27 22:23:21 -0600

    add license links and badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,32 +1,54 @@
+// const license = data.license
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license = 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else if (license = 'Apache 2.0') {
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  } else if (license = 'ISC') {
+    return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+  } else if (license = 'GNU GPLv3') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  } else {
+    return ''
+  }
+};
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  if (license = 'MIT') {
+    return '[https://choosealicense.com/licenses/mit/](https://choosealicense.com/licenses/mit/)';
+  } else if (license = 'Apache 2.0') {
+    return '[https://choosealicense.com/licenses/apache-2.0/](https://choosealicense.com/licenses/apache-2.0/)';
+  } else if (license = 'ISC') {
+    return '[https://choosealicense.com/licenses/isc/](https://choosealicense.com/licenses/isc/)';
+  } else if (license = 'GNU GPLv3') {
+    return '[https://choosealicense.com/licenses/gpl-3.0/](https://choosealicense.com/licenses/gpl-3.0/)';
+  } else {
+    return ''
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 module.exports = (data) => {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
   ## Description
 
   Provide a short description explaining the what, why, and how of your project. Use the following 
   questions as a guide:
   
   - What was your motivation?
-  ${data.motivation}
+  - ${data.motivation}
   - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-  ${data.why}
+  - ${data.why}
   - What problem does it solve?
-  ${data.problem}
+  - ${data.problem}
   - What did you learn?
-  ${data.learn}
+  - ${data.learn}
   
   ## Table of Contents (Optional)
   
@@ -43,7 +65,7 @@ module.exports = (data) => {
   
   What are the steps required to install your project? Provide a step-by-step description of how to 
   get the development environment running.
-  ${data.installInstructions}
+  - ${data.installInstructions}
   
   ## Usage
   
@@ -56,49 +78,31 @@ module.exports = (data) => {
       ![alt text](assets/images/screenshot.png)
       
   
-  ## Credits
-  
-  List your collaborators, if any, with links to their GitHub profiles.
-  
-  If you used any third-party assets that require attribution, list the creators with links to 
-  their primary web presence in this section.
-  
-  If you followed tutorials, include links to those here as well.
-  
   ## License
   
-  The last section of a high-quality README file is the license. This lets other developers know
-   what they can and cannot do with your project. If you need help choosing a license, 
-   refer to [https://choosealicense.com/](https://choosealicense.com/).
-   ${data.license}
-  
-  ---
-  
-  🏆 The previous sections are the bare minimum, and your project will ultimately determine 
-  the content of this document. You might also want to consider adding the following sections.
-  
-  ## Badges
-  
-  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-  
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers 
-  know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). 
-  You may not understand what they all represent now, but you will in time.
-  
-  ## Features
-  
-  If your project has a lot of features, list them here.
+
+   - This program is licensed under ${data.license}
+   - ${renderLicenseLink(data.license)}
+    
   
   ## How to Contribute
   
   If you created an application or package and would like other developers to contribute it, you can 
   include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/)
   is an industry standard, but you can always write your own if you'd prefer.
+  - ${data.contribute}
   
   ## Tests
   
   Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+  - ${data.tests}
 
+  ## Questions
+
+  Contact Information
+  -Github - ${data.github}
+  -Email - ${data.email}
+  Please Email me with any questions or comments you have!
 `;
 };
 
